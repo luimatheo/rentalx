82810e1cebfa541e744729de1f9a053958997662
Luí Matheo <luimatheo@gmail.com>
2021-05-24 13:00:36 -0300

    middlewares and apperror

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
-import 'reflect-metadata';
+import { AppError } from './errors/AppError';
 import { router } from './routes';
 import swaggerFile from './swagger.json';
 
 import './database';
-
 import './shared/container';
 
 const app = express();
@@ -16,5 +17,20 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            message: err.message,
+        });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+        status: 'error',
+        message: `Internal server error - ${err.message}`,
+    });
+});
 
 app.listen(3333, () => console.log('Server is running!'));
